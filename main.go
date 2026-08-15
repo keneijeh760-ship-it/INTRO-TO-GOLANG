724aaf440b72a8758ac0e4f045891f8595d7f584
Kene Ijeh <keneijeh760@gmail.com>
2026-08-15 20:09:55 +0100

    Input ith scan method with a pointer

--- main.go
+++ main.go
@@ -6,7 +6,15 @@ func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets = 50
-	fmt.Printf("Welcome to the %v Booking application", conferenceName)
+	fmt.Printf("Welcome to the %v Booking application\n", conferenceName)
 	fmt.Println("Get your tickets here to attend!")
-	fmt.Printf("There are %c tickets in total and %v tickets still available for purchase.", conferenceTickets, remainingTickets)
+	fmt.Printf("There are %v tickets in total and %v tickets still available for purchase.\n", conferenceTickets, remainingTickets)
+
+	var username string
+	var userTickets int = 2
+
+	fmt.Println("What is your name? ")
+	fmt.Scan(&username)
+
+	fmt.Printf("Hello %v, you have %v tickets!", username, userTickets)
 }
