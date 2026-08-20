04ad34440910b6badc5d221c5b4c70a6d9fb95ca
Kene Ijeh <keneijeh760@gmail.com>
2026-08-20 23:24:30 +0100

    putting user input function in heper file

--- helper.go
+++ helper.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"strings"
+)
+
+func ValidInput(username string, Email string, userTickets int) (bool, bool, bool) {
+	isValidUserName := len(username) >= 2
+	isValidUserTickets := userTickets > 0
+	isValidEmail := strings.Contains(Email, "@")
+	return isValidUserName, isValidUserTickets, isValidEmail
+}
