e59ea492c72e888aeed197a1b259129683c90299
Kene Ijeh <keneijeh760@gmail.com>
2026-08-21 23:32:00 +0100

    putting user input function in heper file

--- main.go
+++ main.go
@@ -92,10 +92,3 @@ func userInput(username string, Email string, userTickets int, city string, rema
 	}
 
 }
-
-func ValidInput(username string, Email string, userTickets int) (bool, bool, bool) {
-	isValidUserName := len(username) >= 2
-	isValidUserTickets := userTickets > 0
-	isValidEmail := strings.Contains(Email, "@")
-	return isValidUserName, isValidUserTickets, isValidEmail
-}
